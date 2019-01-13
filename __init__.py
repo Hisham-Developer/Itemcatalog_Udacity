8549b6a34497f21faf3b267c02eee75c9733dd32
Hisham Alosaimi <33299765+Hisham-Developer@users.noreply.github.com>
2019-01-13 20:19:32 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -16,7 +16,7 @@ from flask import make_response
 import requests
 app = Flask(__name__)
 
-CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())[
+CLIENT_ID = json.loads(open('/var/www/catalog/catalog/client_secrets.json', 'r').read())[
     'web']['client_id']
 APPLICATION_NAME = "Theater Movies Application"
 
